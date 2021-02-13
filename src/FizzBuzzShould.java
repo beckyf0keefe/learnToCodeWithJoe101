fdc99f659c45ece5a88fc042db43883a9e014fa9
Joe <t7051015@live.tees.ac.uk>
2021-02-13 17:15:05 +0000

    Refactor

--- src/FizzBuzzShould.java
+++ src/FizzBuzzShould.java
@@ -3,16 +3,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzShould {
+    private FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
     public void Convert1(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals(1, fizzBuzz.convert(1));
+        assertConvert(1);
     }
 
     @Test
     public void Convert2(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals(2, fizzBuzz.convert(2));
+        assertConvert(2);
     }
 
+    private void assertConvert(int i) {
+        assertEquals(i, fizzBuzz.convert(i));
+    }
 }
